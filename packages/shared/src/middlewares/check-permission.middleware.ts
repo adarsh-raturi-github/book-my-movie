5bcaa0d60b3eda8a2435e85e3f3ac1a882924843
adarsh raturi <adarshraturi@MacBookAir.lan>
2026-07-02 11:13:16 +0530

    Update package code

--- packages/shared/src/middlewares/check-permission.middleware.ts
+++ packages/shared/src/middlewares/check-permission.middleware.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { NotAuthorizeError } from "../errors";
+import { Permission } from "../enums";
 
-export const checkPermission = (permission: string) => {
+export const checkPermission = (permission: Permission) => {
   return (req: Request, _res: Response, next: NextFunction) => {
     if (req.currentUser?.permissions?.includes(permission)) {
       next();
